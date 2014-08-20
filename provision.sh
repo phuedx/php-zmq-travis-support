d6ea27abd34547c59e50d4ced238057be17b7d06
Sam Smith <git@samsmith.io>
2014-08-20 15:03:11 +0100

    Add a basic build environment
    Use Ubuntu 12.04 64-bit, which matches TravisCI's build environment.

---
+++ provision.sh
@@ -0,0 +1,11 @@
+sudo locale-gen en_GB.UTF-8
+
+sudo apt-get update
+sudo apt-get install --yes \
+    build-essential \
+    libtool \
+    autoconf \
+    automake \
+    pkg-config \
+    uuid-dev \
+    git
